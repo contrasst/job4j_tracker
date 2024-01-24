2d61267fc54b63b9fa94149724cf2e2ce239b606
contarsst <mb_matsiyev@mail.ru>
2024-01-24 21:23:14 +0300

    6. Правила Checkstyle [#504981]

---
+++ src/main/java/Checkstyle/Broken.java
@@ -0,0 +1,25 @@
+package Checkstyle;
+
+public class Broken {
+    private int sizeOfEmpty = 10;
+    public String surname;
+    private String name;
+    public static final String newValue = " ";
+
+    void broken() {
+    }
+
+    public void echo() {
+    }
+
+    public void media(Object obj) {
+        if (obj != null) {
+            System.out.println(obj);
+        }
+    }
+
+    public void method(int a, int b, int c, int d, int e, int f, int g, int h) {
+
+    }
+
+}
