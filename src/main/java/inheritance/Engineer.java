4c11f17d7122ffbc5fd991944d46d1e9d6c2c5ef
contarsst <mb_matsiyev@mail.ru>
2024-02-02 18:42:46 +0300

    3. Вызов конструктора родителя super(...) [#6837]

---
+++ src/main/java/inheritance/Engineer.java
@@ -0,0 +1,9 @@
+package inheritance;
+
+public class Engineer extends Profession{
+    private int experience;
+
+    public Engineer(boolean degree) {
+        super(degree);
+    }
+}
---
+++ src/main/java/inheritance/Programmer.java
@@ -0,0 +1,9 @@
+package inheritance;
+
+public class Programmer extends Profession {
+    private String programLanguage;
+
+    public Programmer(boolean degree) {
+        super(degree);
+    }
+}
