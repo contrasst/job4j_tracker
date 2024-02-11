bb59cd0f7710f2466cdcd4e982c972a32330b41d
contrasst <mb_matsiyev@mail.ru>
2024-02-11 12:27:56 +0300

    8. Date. Отображение даты. [#310017]

---
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -0,0 +1,14 @@
+package ru.job4j.tracker;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class StartUI {
+    public static void main(String[] args) {
+        Item time = new Item();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
+        String currenDateTimeFormat = formatter.format(time.getCreated());
+        System.out.println(currenDateTimeFormat);
+
+    }
+}
