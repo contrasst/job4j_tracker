b0f88e01dc754eb506887191ca2fdafe09181d32
contrasst <mb_matsiyev@mail.ru>
2024-02-12 11:00:39 +0300

    3. Массивы и модели. [#174892 #480701]

--- src/main/java/ru/job4j/pojo/Library.java
+++ src/main/java/ru/job4j/pojo/Library.java
@@ -25,7 +25,7 @@ public class Library {
         System.out.println("Clean Code books");
         for (int i = 0; i < books.length; i++) {
             Book book = books[i];
-            if(books[1].equals(book)) {
+            if (books[1].equals(book)) {
                 System.out.println(book.getName() + " - " + book.getPages());
             }
         }
