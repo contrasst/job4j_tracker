a0960b324020512e7b8a21c5ceed34424a88dc64
contrasst <mb_matsiyev@mail.ru>
2024-02-12 19:39:15 +0300

    5. Tracker - хранилище [#396]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,5 +1,7 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -12,46 +14,36 @@ public class Tracker {
     }
 
     public Item[] findByName(String key) {
-        Item[] result = new Item[items.length];
+        Item[] result = new Item[size];
         int count = 0;
-        for (Item item : items) {
-            if (item != null && item.getName() != null && item.getName().equals(key)) {
-                result[count] = item;
-                count++;
-            }
+        for (int i = 0; i < size; i++) {
+           if(items[i].getName().equals(key)) {
+               result[i] = items[i];
+               count++;
+           }
         }
-        Item[] foundItems = new Item[count];
-        System.arraycopy(result, 0, foundItems, 0, count);
-        return foundItems;
+        return Arrays.copyOf(result, count);
     }
 
     public Item[] findAll() {
-        int count = 0;
-        for (Item item : items) {
-            if (item != null) {
-                count++;
-            }
-        }
-        Item[] nonNull = new Item[count];
-        int index = 0;
-        for (Item item : items) {
-            if (item != null) {
-                nonNull[index] = item;
-                index++;
-            }
-        }
-        return nonNull;
+        return Arrays.copyOf(items, size);
     }
 
     public Item findById(int id) {
-        Item result = null;
         for (int index = 0; index < size; index++) {
             Item item = items[index];
             if (item.getId() == id) {
-                result = item;
-                break;
+                return item;
             }
         }
-        return result;
+        return null;
     }
 }
+
+
+
+
+
+
+
+
