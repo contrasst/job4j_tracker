5d74261b23270989c613e5b5808e0c02e2fc04e3
contrasst <mb_matsiyev@mail.ru>
2024-02-12 09:12:52 +0300

    2. Модель данных. [#174683]

---
+++ src/main/java/ru/job4j/pojo/College.java
@@ -0,0 +1,10 @@
+package ru.job4j.pojo;
+
+public class College {
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.setDataJoin("22.07.2006");
+        student.setGroup(2);
+        student.setfIO("Sidorov Ivan Genadievych");
+    }
+}
---
+++ src/main/java/ru/job4j/pojo/Student.java
@@ -0,0 +1,31 @@
+package ru.job4j.pojo;
+
+public class Student {
+    private String fIO;
+    private String dataJoin;
+    private int group;
+
+    public int getGroup() {
+        return group;
+    }
+
+    public void setGroup(int group) {
+        this.group = group;
+    }
+
+    public String getDataJoin() {
+        return dataJoin;
+    }
+
+    public void setDataJoin(String dataJoin) {
+        this.dataJoin = dataJoin;
+    }
+
+    public String getfIO() {
+        return fIO;
+    }
+
+    public void setfIO(String fIO) {
+        this.fIO = fIO;
+    }
+}
