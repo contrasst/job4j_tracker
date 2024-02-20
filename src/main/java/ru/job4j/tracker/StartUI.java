969a4c4671cff69813712b214fc5a3b82f420d76
contrasst <mb_matsiyev@mail.ru>
2024-02-20 13:36:33 +0300

    3.0. Реализация класса StartUI. Вывод меню [#500743 #481830]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -7,6 +7,7 @@ public class StartUI {
         boolean run = true;
         while (run) {
             showMenu();
+            System.out.print("Выбрать: ");
             int select = Integer.parseInt(scanner.nextLine());
             if (select != 6) {
                 System.out.println("Пользователь выбрал: " + select);
@@ -18,11 +19,11 @@ public class StartUI {
 
     private void showMenu() {
         String[] menu = {
-                "Add new Item", "Show all items", "Edit item",
-                "Delete item", "Find item by id", "Find items by name",
-                "Exit Program"
+                "Добавить новую заявку", "Показать все заявки", "Изменить заявку",
+                "Удалить заявку", "Показать заявку по id", "Показать заявки по имени",
+                "Завершить программу"
         };
-        System.out.println("Menu:");
+        System.out.println("Меню:");
         for (int i = 0; i < menu.length; i++) {
             System.out.println(i + ". " + menu[i]);
         }
