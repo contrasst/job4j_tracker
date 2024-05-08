939079d5e72601757c6ba5fda2ebcdd4a3a4491b
contrassst <mb_matsiyev@mail.ru>
2024-05-08 17:33:21 +0300

    2. Рефакторинг - Шаблон Декоратор для валидатора. [#34117]

--- src/main/java/ru/job4j/tracker/ValidateInput.java
+++ src/main/java/ru/job4j/tracker/ValidateInput.java
@@ -1,8 +1,23 @@
 package ru.job4j.tracker;
 
+
 import ru.job4j.input.ConsoleInput;
+import ru.job4j.input.Input;
+import ru.job4j.output.Output;
 
 public class ValidateInput extends ConsoleInput {
+    private final Output output;
+    private final Input input;
+
+    public ValidateInput(Output output, Input input) {
+        this.output = output;
+        this.input = input;
+    }
+
+    @Override
+    public String askStr(String question) {
+        return input.askStr(question);
+    }
 
     @Override
     public int askInt(String question) {
