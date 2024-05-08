2d9b1121c2af8b25d45c0ff73732244acfb68fea
contrassst <mb_matsiyev@mail.ru>
2024-05-08 17:27:40 +0300

    1.1. Тесты на StartUI. [#223313]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -14,6 +14,28 @@ import static org.assertj.core.api.Assertions.assertThat;
 class StartUITest {
 
     @Test
+    void whenInvalidText() {
+        Output output = new StubOutput();
+        Input input = new MockInput(
+                new String[] {"-1", "0"}
+        );
+        Tracker tracker = new Tracker();
+        UserAction[] actions = new UserAction[]{
+                new ExitAction(output)
+        };
+        new StartUI(output).init(input, tracker, actions);
+        String ln = System.lineSeparator();
+        assertThat(output.toString()).isEqualTo(
+                "Меню:" + ln
+                        + "0. Завершить программу" + ln
+                        + "Неверный ввод, вы можете выбрать: 0 .. 0" + ln
+                        + "Меню:" + ln
+                        + "0. Завершить программу" + ln
+                        + "=== Завершение программы ===" + ln
+        );
+    }
+
+    @Test
     void whenCreateItem() {
         Output output = new ConsoleOutput();
         Input input = new MockInput(
